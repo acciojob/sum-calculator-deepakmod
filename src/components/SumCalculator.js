5c5dc05e1114fe2f372cd1c2669e4b71b1cea4e2
Deepak Modanwal <deepakmodanwal404@gmail.com>
2023-06-23 10:23:05 +0530

    one

---
+++ src/components/SumCalculator.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const SumCalculator = () => {
+    const [sum, setSum] = useState(0);
+    return (
+        <div>
+            <h1>Sum Calculator</h1>
+            <input type="number" onChange={(e)=>{setSum(sum+parseInt(e.target.value))}} ></input>
+            <p>Sum: {sum} </p>
+        </div>
+    );
+};
+
+export default SumCalculator;
